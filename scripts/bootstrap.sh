b3ca9ec76f2759a8dbf6d290eab38a529ed22b33
Aisha Tammy <gentoo@aisha.cc>
2020-03-03 14:00:33 -0500

    added bootstrap script

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+
+if [ $(id -u) -ne 0 ] ; then
+    echo "this script must be run as root"
+    exit 1
+fi
+
+echo "We are going to first prepare the OpenBSD install to support AnsiMail"
+echo "The following files are going to be changed:"
+echo "/etc/installurl: The default package url is going to be changed to https://cdn.openbsd.org/pub/OpenBSD"
+echo "/etc/doas.conf: We will permit all member of :wheel to doas without password, change this afterwards"
+
+echo 'permit nopass :wheel' > /etc/doas.conf
+echo 'https://cdn.openbsd.org/pub/OpenBSD' > /etc/installurl
+pkg_add -xz ansible
