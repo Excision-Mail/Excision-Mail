1334e8252e09cf59455ec0ba9561585ab5cd17a1
Aisha Tammy <ansimail@aisha.cc>
2020-03-29 20:03:15 -0400

    finished configuration of multi-domain GPG-WKS server

--- roles/dovecot/files/sieve-shell/wks-server.sh
+++ roles/dovecot/files/sieve-shell/wks-server.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 exec /usr/local/bin/gpg-wks-server -v --receive \
-		--header X-WKS-Loop=wks.{{ domain }} \
+		--header X-WKS-Loop=wks.multidomain.loop \
 		--from "${1}" \
 		--send
 # we don't care about the exit status in this case
