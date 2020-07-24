dcbb98a37a623860cfb34e76165a58a827b3f3eb
Aisha Tammy <gentoo@aisha.cc>
2020-07-23 20:59:43 -0400

    davical and roundcube are working pretty well now

---
+++ roles/roundcube/files/newmail_notifier.config.inc.php
@@ -0,0 +1,15 @@
+<?php
+
+// Enables basic notification
+$config['newmail_notifier_basic'] = true;
+
+// Enables sound notification
+$config['newmail_notifier_sound'] = true;
+
+// Enables desktop notification
+$config['newmail_notifier_desktop'] = true;
+
+// Desktop notification close timeout in seconds
+$config['newmail_notifier_desktop_timeout'] = 10;
+
+?>
